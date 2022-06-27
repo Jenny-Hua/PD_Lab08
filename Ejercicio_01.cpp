1bf9f0388f0916dc9c6c37c9c0862c749c36031b
Jenny-Hua <jhuancaan@unsa.edu.pe>
2022-06-27 12:05:58 -0500

    First version

--- Ejercicio_01.cpp
+++ Ejercicio_01.cpp
@@ -1,21 +1,53 @@
 //
 // Lab 08 - Máximo número de hamburguesas
+// Versión TOP DOWN
 //
 #include "bits/stdc++.h"
 
 using  namespace  std;
 
-const int INF = INT_MIN;
+const int max_kg = 4096;
+
+// Función Recursiva
 int maxHamburguesas(int m, int n, int T)
 {
-    if (T < m)
-        return INF;
+    int minimo = min(m, n);
+    if (T < minimo)
+        return INT_MIN;
     else if (T == m || T == n)
         return 1;
-    else
+    else {
+        int mHamb = maxHamburguesas(m, n, T-m);
+        int nHamb = maxHamburguesas(m, n, T-n);
+        int maxNumHamb = max(mHamb, nHamb) + 1;
+        return  maxNumHamb;
+    }
+}
 
+// Función con Memoización
+int maxHamburguesasMemoi(int m, int n, int T)
+{
+    vector<vector<int>> memo (2, vector<int> (T + 1, 1));
 
-    return -1;
+    if (T == m || T == n)
+        return 1;
+
+    int minimo = min(m, n);
+    if (T < minimo)
+        return -1;
+
+    //    if (memo[0][T] != -1  || memo[1][T] != -1 )
+//        return  max(memo[0][T], memo[1][T]);
+
+    // Almacenando valores de cada tabla
+    int mHamb = maxHamburguesas(m, n, T-m);
+    memo[0][T] = mHamb;
+    int nHamb = maxHamburguesas(m, n, T-n);
+    memo[1][T] = nHamb;
+
+    int maxNumHamb = max(memo[0][T], memo[1][T]) + 1;
+
+    return  maxNumHamb;
 }
 
 int main()
@@ -36,10 +68,10 @@ int main()
         3332
     };
 
-    cout << setw(10) << "Entrada" << setw(6) << "Salida" << setw(10) << "Resultado" << setw(6) << "Test" <<endl;
-    for (int i = 0; i < 5; ++i) {
+    cout << setw(10) << "Entrada" << setw(8) << "Salida" << setw(10) << "Resultado" << setw(8) << "Test" <<endl;
+    for (int i = 0; i < 4; ++i) {
         cout << input[i][0] << " " << input[i][1] << " " << input[i][2];
-        int resultado = maxHamburguesas(input[i][0], input[i][1], input[i][2]);
+        int resultado = maxHamburguesasMemoi(input[i][0], input[i][1], input[i][2]);
         cout << setw(6) << output[i] << setw(10) << resultado
              << setw(6);
         if (resultado == output[i])
